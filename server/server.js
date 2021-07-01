a1bca404228f10ed860c865c753049d1462da053
William Berry <76190311+wberry86@users.noreply.github.com>
2021-07-01 15:21:19 -0500

    setup backend

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -9,7 +11,8 @@ const app = express();
 // new apollo server with schema data passed in
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 // integrate apollo server with express application as middleware
